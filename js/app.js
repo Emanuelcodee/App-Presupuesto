526022d599aed277676bafdd459dc962c7ad5d0a
Emanuel Vanegas <emanuelvc309@gmail.com>
2021-03-02 23:11:50 -0500

    Se agrego el listado de egresos e ingresos y el formato de moneda

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 //ARREGLOS DE INGRESOS & EGRESOS
 const ingresos = [
     new Ingreso ('Salario', 2000),
-    new Ingreso ('Venta coche', 1500),
+    new Ingreso ('Venta coche', 1500)
 ];
 
 const egresos = [
     new Egreso ('Renta Departamento', 900),
-    new Egreso ('Comida', 4000)
+    new Egreso ('Comida', 400)
 ];
 
 
@@ -32,11 +32,23 @@ let totalEgresos = ()=> {
     return totalEgreso;
 }
 
+
+//FUNCION QUE CALCULA EL PRESUPUESTO TOTAL Y EL PORCENTAJE DE EGRESO Y LO ASIGNA MEDIANTE LA PROPEIDAD INNER
 let cargarCabecero = ()=> {
-    let presupuestoTotal = totalIngresos() - totalIngresos();
+    let presupuesto = totalIngresos() - totalEgresos();
     let porcentajeEgreso = totalEgresos() / totalIngresos();
-    document.getElementById('presupuesto').innerHTML = presupuestoTotal;
-    document.getElementById('porcentaje').innerHTML = porcentajeEgreso;
-    document.getElementById('ingresos').innerHTML = totalIngresos();
-    document.getElementById('egresos').innerHTML = totalEngresos();
+    document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
+    document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
+}
+
+//ESTA FUNCION DA UN FORMATO DE MONEDA EN DOLARES
+const formatoMoneda = (valor)=> {
+    return valor.toLocaleString('en-US', {style: 'currency', currency:'USD', minimumFractionDigits:2});
+}
+
+//FORMATO DE PORCENTAJE
+const formatoPorcentaje = (valor)=> {
+    return valor.toLocaleString('en-US', {style:'percent', minimumFractionDigits:2});
 }
